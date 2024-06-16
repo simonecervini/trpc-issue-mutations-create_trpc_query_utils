2949eb473890b33a2cd30b4857b749f0a5d64216
Simone Cervini <sc.cervini@gmail.com>
2024-06-16 14:48:28 +0200

    Add minimal repro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,18 @@
-import Link from "next/link";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { QueryClient } from "@tanstack/react-query";
+import { createTRPCQueryUtils } from "@trpc/react-query";
+import { api } from "~/trpc/react";
 
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
-import styles from "./index.module.css";
+const trpcClient = api.createClient({ links: [] });
+const queryClient = new QueryClient();
+const clientUtils = createTRPCQueryUtils({ queryClient, client: trpcClient });
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+type ThisIsCorrectlyTyped = typeof clientUtils.post.getLatest;
+//   ^?
 
-  return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>
-          Create <span className={styles.pinkSpan}>T3</span> App
-        </h1>
-        <div className={styles.cardRow}>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>First Steps →</h3>
-            <div className={styles.cardText}>
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>Documentation →</h3>
-            <div className={styles.cardText}>
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className={styles.showcaseContainer}>
-          <p className={styles.showcaseText}>
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
+type ThisIsNotCorrectlyTyped = typeof clientUtils.post.create;
+//   ^?
 
-        <CrudShowcase />
-      </div>
-    </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest();
-
-  return (
-    <div className={styles.showcaseContainer}>
-      {latestPost ? (
-        <p className={styles.showcaseText}>
-          Your most recent post: {latestPost.name}
-        </p>
-      ) : (
-        <p className={styles.showcaseText}>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
-  );
+export default function Page() {
+  return null;
 }
